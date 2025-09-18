4a0e78e4eab522a597bc34e4518de97b2b93fb50
Andy Grunwald <andygrunwald@gmail.com>
2025-09-18 22:09:37 +0200

    Fix "Error return value of `*.Close` is not checked (errcheck)"

--- app/io/file.go
+++ app/io/file.go
@@ -72,14 +72,14 @@ func CopyFile(src, dst string) error {
 	if err != nil {
 		return fmt.Errorf("could not open source file: %w", err)
 	}
-	defer sourceFile.Close()
+	defer func() { _ = sourceFile.Close() }()
 
 	// Create destination file
 	destFile, err := os.Create(dst)
 	if err != nil {
 		return fmt.Errorf("could not create destination file: %w", err)
 	}
-	defer destFile.Close()
+	defer func() { _ = destFile.Close() }()
 
 	// Copy contents
 	_, err = io.Copy(destFile, sourceFile)
